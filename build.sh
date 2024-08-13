a7e86c0009fc9bf1b2f86ce724147c92b9d96953
Manuel Garcia Roura <manuel.garciaroura@westinghouse.com>
2024-08-13 11:39:50 +0200

    Started implementing the code as a library.

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+export PETSC_DIR=/lib/petscdir/petsc3.12
+export SLEPC_DIR=/lib/slepcdir/slepc3.12
+export PETSC_ARCH=x86_64-linux-gnu-real
+
+cd src
+make clean
+make
+cd ..
